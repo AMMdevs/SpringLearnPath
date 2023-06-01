ec047b7557a3544c4e2a0e84b3f744b562c53163
Amparo Manrique <amparomanriquem@gmail.com>
2023-06-01 17:53:33 +0200

    Added Delete, Put and Get controllers

--- src/main/java/ammanriq/spring_learn_path/HelloWorld.java
+++ src/main/java/ammanriq/spring_learn_path/HelloWorld.java
@@ -1,15 +1,41 @@
 package ammanriq.spring_learn_path;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Scanner;
 
 @RestController
 @RequestMapping(path = "hello")
 public class HelloWorld {
-    
-    @GetMapping
+    private String name = null;
+
+    @GetMapping ("world")
     public String helloWorld(){
         return "Hello World!";
     }
+
+    @GetMapping
+    public String helloName(){
+        if (name == null) {
+            return "Not name found.";
+        } else {
+            return "Hello " + name ;
+        }
+    }
+
+    @PutMapping
+    public void setName(@RequestBody String name){
+        this.name = name;
+    }
+
+    @DeleteMapping
+    public void deleteName(){
+        name = null;
+    }
+
+
+
+
+
 }
